224b904db9b559962b09a232ed7654e0feacfc28
A-Danil <dan190598@gmail.com>
2022-09-10 08:09:08 +0300

    add animation

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,6 +1,9 @@
 import { DeleteOutlined } from '@ant-design/icons';
 import classNames from 'classnames';
 import { useDispatch } from 'react-redux';
+import { motion } from "framer-motion";
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 import { deleteTodo, checkTodo } from '../../redux/actions';
 
@@ -8,20 +11,47 @@ import './Item.scss';
 
 function Item({data, id, time, completed}) {
   const dispatch = useDispatch();
+  const [isDelete, setDelete] = useState(false);
+
+  const itemVariant = {
+    hidden: { y: 10, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    },
+  };
 
   return (
-    <div className={classNames('item', {
-      'item--done' : completed,
-      'item--big' : data.length > 150
-      })}>
-      <h3 className={classNames('', {'done' : completed})}>{data}</h3>
-      <DeleteOutlined className='item__del' onClick={()=>dispatch(deleteTodo(id))}/>
-      <label>
-        <input type="checkbox" checked={completed} onChange={()=> dispatch(checkTodo(id))}/>
-        <span className='checkbox'></span>
-      </label>
-      <span className='item__time'>{time}</span>
-    </div>
+    <AnimatePresence>
+      {!isDelete &&
+        <motion.div 
+          variants={itemVariant} 
+          initial={{opacity: 0}} 
+          animate={{opacity: 1}} 
+          exit={{opacity: 0}} 
+          className={classNames('item', {
+            'item--done' : completed,
+            'item--big' : data.length > 150
+          })}
+        >
+
+          <h3 className={classNames('', {'done' : completed})}>{data}</h3>
+          <motion.span whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
+            <DeleteOutlined className='item__del' onClick={()=> {
+              setDelete(!isDelete);
+              setTimeout(()=>{
+                dispatch(deleteTodo(id))
+              }, 500)
+              }}/>
+          </motion.span>
+          <label>
+            <input type="checkbox" checked={completed} onChange={()=> dispatch(checkTodo(id))}/>
+            <span className='checkbox'></span>
+          </label>
+          <span className='item__time'>{time}</span>
+
+      </motion.div>}
+    </AnimatePresence>
   )
 }
 
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,15 +1,17 @@
 import { RocketOutlined, PlusOutlined } from '@ant-design/icons';
-import { Input, Empty } from 'antd';
+import { Input, Empty, Alert } from 'antd';
 import { format } from 'date-fns';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { orderBy } from 'lodash';
+import { motion } from "framer-motion";
+import { AnimatePresence } from 'framer-motion';
+import classNames from 'classnames';
 
 import { addTodo } from '../../redux/actions';
 import { Item, Button } from '../index';
 
 import './Todo.scss';
-import classNames from 'classnames';
 
 function Todo() {
   const [inputText, setInputText] = useState('');
@@ -17,33 +19,77 @@ function Todo() {
 
   const list = useSelector((state)=> state.todoReducers.list);
   const dispatch = useDispatch();
-
+  
+  
   function dispatchAddTodo(){
     setTime(format(new Date(),'H:mm'));
     dispatch(addTodo(inputText, time));
     setInputText('');
   }
- 
+  
+  function handleChange(e){
+    setInputText(e.target.value); 
+    setTime(format(new Date(),'H:mm'));
+  }
+
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
   return (
     <div className="todo">
-      <div className='todo__content'>
+      <motion.div className='todo__content'
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
         <h1 className="todo__header">Add you list here <RocketOutlined /></h1>
 
         <Input
-          className='todo__input'
-          onChange={(e)=>{setInputText(e.target.value); setTime(format(new Date(),'H:mm'))}}
+          className={classNames('todo__input', {'todo__input--error' : inputText.length === 250})}
+          maxLength="250"
+          onChange={(e)=>{handleChange(e)}}
           onKeyDown={(e)=>{if(e.key === "Enter" && inputText){dispatchAddTodo()}}}
           size="large"
           value={inputText}
           placeholder="✍️ Add items..."
-          suffix={ <PlusOutlined onClick={()=> {inputText && dispatchAddTodo()}} />}
+          suffix={
+            <motion.span
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <PlusOutlined onClick={()=> {inputText && dispatchAddTodo()}} />
+            </motion.span>
+          }
+          prefix={inputText.length === 250 &&
+            <AnimatePresence>
+              <motion.span 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <Alert
+                  className='error'
+                  message="250 symbols lymit"
+                  type="warning"
+                />
+              </motion.span>
+            </AnimatePresence>
+            }
         />
 
-        <div className={classNames('todo__items', {
-          'todo__items--scroll' : list.length > 4 || list.some(el=> el.data.length > 150)
-        })}>
+        <div className={classNames('todo__items', {'todo__items--scroll' : list.length > 4 || list.some(el=> el.data.length > 150)})}>
           { list.length > 0 ?
-            orderBy(list, ["time"], ["desc"]).map((item) => (
+            orderBy(list, ["id"], ["desc"]).map((item) => (
               <Item key={item.id} {...item}/>
             )) :
             <Empty description="Add any item" />
@@ -56,7 +102,7 @@ function Todo() {
           </div> :
           null
         }
-      </div>
+      </motion.div>
     </div>
   )
 }
